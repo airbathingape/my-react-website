c9e495362c6f2b608749d23b9135e129f33d7405
Daniel Domingo <domingo.daniel21@gmail.com>
2022-09-15 18:31:15 +0900

    added footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App mt-5">
-        <h1>Shoe brands that we carry</h1>
-        <ul>
-          <li>Jordan</li>
-          <li>Nike</li>
-          <li>Adidas</li>
-          <li>Bape</li>
-        </ul>
+    <div className='App'>
+      <h1>Brands that we carry</h1>
+      <ul>
+        <li>Nike</li>
+        <li>Jordan</li>
+        <li>Bape</li>
+        <li>Adidas</li>
+      </ul>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div id="footer">
+      <p>© 2022 AirBathingApe Development. All rights reserved.</p>
+    </div>
+  );
+}
+
+export default Footer;
