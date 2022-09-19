08ffcaf34afc527190eabca6c2069057a0a34562
Daniel Domingo <domingo.daniel21@gmail.com>
2022-09-19 18:02:13 +0900

    added hover effects

--- src/App.js
+++ src/App.js
@@ -25,6 +25,10 @@ function App() {
         <img src={logo} className="yeezy" alt='pic' />
       </div>
       <div class="div3">
+
+<div className="navBorder">
+
+
         <div className="navbar">
           <select class="form-select" aria-label="Default select example">
             <option selected>Size:</option>
@@ -39,18 +43,21 @@ function App() {
             </option>
           </select>
         </div>
-        <div className="bids d-flex">
-          <div className="place">Place bid</div>
-          <div className="buy">Buy for</div>
+        <div className="bids">
+          <div className="place"><strong>Place bid</strong></div>
+          <div className="buy"><strong>Buy for ¥1,486,700</strong></div>
         </div>
+        <div className="priceBorder"></div>
         <div className="ask">
-          <p>sell for 940,400 or more</p>
+          <p>sell for ¥940,400 or more</p>
         </div>
+</div>
 
 
-        <div className="last sale">
+
+        <div className="lastSale">
           <div className="sale">
-            <p>last sale:</p>
+            <p>Last Sale:</p>
           </div>
           <div className="mainPrice d-flex">
             <div className="last">
@@ -62,9 +69,9 @@ function App() {
               <div className="ax">View Sales</div>
             </div>
           </div>
-            <div className="percent">
-              <p>▲ ¥29,600 (3.1%)</p>
-            </div>
+          <div className="percent">
+            <p>▲ ¥29,600 (3.1%)</p>
+          </div>
         </div>
       </div>
       <div class="div4"> </div>
