eb5450361e3086b1763efb3e89fa48235940ac45
Daniel Domingo <domingo.daniel21@gmail.com>
2022-09-19 13:04:51 +0900

    finished styling top part

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import './App.css'
 
 function App() {
   return (
-    <div className='App'>
-      <div className='Center'>
-      <h1>Brands that we carry</h1>
-      <ul>
-        <li>Nike</li>
-        <li>Jordan</li>
-        <li>Bape</li>
-        <li>Adidas</li>
-      </ul>
+    <div class="parent">
+      <div class="div1  d-flex justify-content-between">
+        <div className="top"><p>
+          Home / Sneakers / Air Jordan / 3 / <span className='underline'>Jordan 3 Retro Fire Red (2022)</span>
+        </p>
+          <h2>Jordan 3 Retro</h2>
+          <h3>Fire Red</h3>
+          <div className='d-flex'>
+          <p className='shoeDetails'>verified authentic</p><p className='shoeDetails2'>condition new</p>
+          </div>
+        </div>
+        <div className="icons"> icons</div>
       </div>
-
+      <div class="div2"> </div>
+      <div class="div3"> </div>
+      <div class="div4"> </div>
+      <div class="div5"> </div>
     </div>
   );
 }
