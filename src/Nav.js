780ef3223d1ad983f67261a3ebe40fbbe65c393a
Daniel Domingo <domingo.daniel21@gmail.com>
2022-09-19 21:54:33 +0900

    added top stuff

--- src/Nav.js
+++ src/Nav.js
@@ -5,11 +5,21 @@ function Nav() {
   return (
     <nav className='nav'>
       <div className="Nav-header">
-        <div>
-          <img src={logo} className="App-logo" alt="logo" />
+        <div class="main">
+          <div class="form-group has-search">
+            <span class="fa fa-search form-control-feedback"></span>
+            <input type="text" class="form-control" placeholder="Search for brand, color, etc." />
+          </div>
         </div>
-        <div className='m-4'>
-          <h1>Daniel's Shoe Store</h1>
+        <div className='d-flex navFont'>
+          <p className='navMargin'>Browse</p>
+          <p className='navMargin'>News</p>
+          <p className='navMargin'>About</p>
+          <p className='navMargin'>Help</p>
+          <p className='navMargin'>Sell</p>
+          <p className='navMargin'>bell</p>
+          <p className='ax navMargin'>Log In</p>
+          <p className='ax navMargin'>Sign Up</p>
         </div>
       </div>
     </nav>
