566b2ee776ee0752534adb17508d16eed5129301
Daniel Domingo <domingo.daniel21@gmail.com>
2022-09-19 16:21:00 +0900

    finished top half

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import './App.css'
 function App() {
   return (
     <div class="parent">
-      <div class="div1  d-flex justify-content-between">
+      <div class="div1 d-flex justify-content-between">
         <div className="top"><p>
           Home / Sneakers / Nike / Other / Premium / <span className='underline'>Nike Air Yeezy 2 Solar Red</span>
         </p>
           <h2>Air Yeezy 2</h2>
           <h3>Solar Red</h3>
           <div className='d-flex'>
-          <p className='shoeDetails'>Verified Authentic</p><p className='shoeDetails2'>Condition New</p>
+            <p className='shoeDetails'>Verified Authentic</p>
+            <p className='shoeDetails2'>Condition: New</p>
           </div>
         </div>
-        <div className="icons">
-        <i class="fa-solid fa-circle-plus"></i>
-        <i class="fa-solid fa-heart"></i>
-        <i class="fa-solid fa-arrow-right-from-bracket"></i>
+        <div className="icons d-flex flex-column-row">
+          <i class="fa-solid fa-circle-plus"></i>
+          <i class="fa-solid fa-heart iconMargin"></i>
+          <i class="fa-solid fa-arrow-right-from-bracket iconMargin"></i>
         </div>
       </div>
       <div class="div2"> </div>
