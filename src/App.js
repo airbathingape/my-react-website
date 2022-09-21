4211c9a563981893214e6c313cef979f27e0c8c6
Daniel Domingo <domingo.daniel21@gmail.com>
2022-09-21 15:27:39 +0900

    moved main content into it's own component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 import logo from './solar.png';
+import MainContent from './MainContent.js'
 
 function App() {
   return (
@@ -24,48 +25,7 @@ function App() {
           <img src={logo} className="yeezy" alt='pic' />
         </div>
         <div class="div3">
-          <div className="navBorder">
-            <div className="navbar">
-              <select class="form-select" aria-label="Default select example">
-                <option selected>Size:</option>
-                <option value="1">
-                  <p>US M 7 ¥2,513,700</p>
-                </option>
-                <option value="2">
-                  <p>US M 8 ¥2,746,900</p>
-                </option>
-                <option value="3">
-                  <p>US M 9 ¥1,953,100</p>
-                </option>
-              </select>
-            </div>
-            <div className="bids">
-              <div className="place"><strong>Place bid</strong></div>
-              <div className="buy"><strong>Buy for ¥1,486,700</strong></div>
-            </div>
-            <div className="priceBorder"></div>
-            <div className="ask">
-              <strong><p>Sell for ¥940,400 or Ask for More</p></strong>
-            </div>
-          </div>
-          <div className="lastSale">
-            <div className="sale">
-              <p>Last Sale:</p>
-            </div>
-            <div className="mainPrice d-flex">
-              <div className="last">
-                <p>¥970,800</p>
-              </div>
-              <div className="boxes">
-                <div className="ax">View Asks</div>
-                <div className="ax">View Bids</div>
-                <div className="ax">View Sales</div>
-              </div>
-            </div>
-            <div className="percent">
-              <p>▲ ¥29,600 (3.1%)</p>
-            </div>
-          </div>
+          <MainContent />
         </div>
         <div class="div4">
           <div className='bottomBorder'></div>
