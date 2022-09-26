df03382c7364fed9e8aae4e4013cab6423e15330
Daniel Domingo <domingo.daniel21@gmail.com>
2022-09-26 12:34:21 +0900

    added more shoes

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,7 +7,7 @@ function Cards(props) {
       <div className='card'>
         <img src={`../images/${props.item.imageUrl}`} alt="Off-White" className='cardPhoto' />
         <div className='container'>
-          <p className='cardSize'>{props.item.title} {props.item.colorway}</p>
+          <p className='cardSize'>{props.item.title}<br></br>{props.item.colorway}</p>
           <p className='lastCard'>Lowest Ask</p>
           <p className='last'>{props.item.price}</p>
           <p className='shoeDetails3'>Last Sale: {props.item.lastSale}</p>
--- src/components/data.js
+++ src/components/data.js
@@ -7,9 +7,39 @@ const data = [
     lastSale: "¥691,100"
   },
   {
-    title: "Jordan 1",
-    colorway: "Off White Chicago",
-    imageUrl: "offwhite.png"
+    title: "Jordan 4 Retro",
+    colorway: "Lightning",
+    imageUrl: "lightning.png",
+    price: "¥38,100",
+    lastSale: "¥40,900"
+  },
+  {
+    title: "Jordan 1 Low",
+    colorway: "Travis Scott x Fragment",
+    imageUrl: "frags.jpg",
+    price: "¥176,300",
+    lastSale: "¥213,400"
+  },
+  {
+    title: "Nike Air Presto",
+    colorway: "Off White",
+    imageUrl: "presto.png",
+    price: "¥379,800",
+    lastSale: "¥343,800"
+  },
+  {
+    title: "Jordan 5",
+    colorway: "Off White",
+    imageUrl: "off5.jpg",
+    price: "¥78,700",
+    lastSale: "¥86,000"
+  },
+  {
+    title: "Jordan 5",
+    colorway: "Off White",
+    imageUrl: "off5.jpg",
+    price: "¥78,700",
+    lastSale: "¥86,000"
   },
 ]
 
